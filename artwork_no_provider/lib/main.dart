5df084a034ee0a8e2fe7321df157c0a2128cad2e
Nghj <nghint.it@gmail.com>
2026-03-20 12:48:05 +0700

    done

--- artwork_no_provider/lib/main.dart
+++ artwork_no_provider/lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'db/database_helper.dart';
 import 'providers/shared_preference_provider.dart';
 import 'ui/login_screen.dart';
 import 'ui/list_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await DatabaseHelper.instance.database;
   final prefs = await SharedPreferences.getInstance();
   runApp(ProviderScope(
     overrides: [sharedPreferencesProvider.overrideWithValue(prefs)],
